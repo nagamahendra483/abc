a7ca22f436ca5c61e1a7f3ba4dd1b08ebad3f740
nagamahendra483 <50797898+nagamahendra483@users.noreply.github.com>
2021-08-31 23:11:41 +0530

    Update secondapp.js

--- secondapp.js
+++ secondapp.js
@@ -1,16 +1,9 @@
-function load(){
-    const tableDiv = document.getElementById('table-body');
-    // var table = document.getElementById('table-body');
-
-	// var rowCount = table.rows.length;
-	// var row = table.insertRow(rowCount);
-    // const title= localStorage.getItem("title"), option1=localStorage.getItem("option1"), option2=localStorage.getItem("option2"),
-    // option3=localStorage.getItem("option3"),option4=localStorage.getItem("option4"), correct=localStorage.getItem("correct");
-    const data=JSON.parse(localStorage.getItem("obj"));
-    // addtolist(data);
-    // displayAll();
-
-    const content = `<tr>
+function load() {
+  const tableDiv = document.getElementById('table-body');
+  const values = JSON.parse(localStorage.getItem("obj"));
+  let text = "";
+  values.forEach(data => {
+    text += `<tr>
       <td>${data.title}</td>
       <td>${data.option1}</td>
       <td>${data.option2}</td>
@@ -18,63 +11,15 @@ function load(){
       <td>${data.option4}</td>
       <td>${data.correct}</td>
       <td><button class="btn btn-primary btn-sm" type="button" onClick="removeRow(this)">Remove</button></td>
-    </tr>`;
-    
-    tableDiv.innerHTML += content; 
+    </tr>`
+  });
+  tableDiv.innerHTML = text;
 }
 
-// list=[];
-// function addtolist(data1){
-//   list.push(data1);
-  
-
-// }
-
-// function displayAll(){
-//   const tableDiv = document.getElementById('table-body');
-//     for(let dat in list){
-//       const content = `<tr>
-//       <td>${dat.title}</td>
-//       <td>${dat.option1}</td>
-//       <td>${dat.option2}</td>
-//       <td>${dat.option3}</td>
-//       <td>${dat.option4}</td>
-//       <td>${dat.correct}</td>
-//       <td><button class="btn btn-primary btn-sm" type="button" onClick="removeRow(this)">Remove</button></td>
-//     </tr>`;
-    
-//     tableDiv.innerHTML += content; 
-
-//     }
-// }
-// function myFunction() {
-//     var table = document.getElementById("table-body");
-//     var row = table.insertRow(0);
-//     var cell1 = row.insertCell(0);
-//     var cell2 = row.insertCell(1);
-//     var cell3 = row.insertCell(2);
-//     var cell4 = row.insertCell(3);
-//     var cell5 = row.insertCell(4);
-//     var cell6 = row.insertCell(5);
-//     cell1.innerHTML = localStorage.getItem("title");
-//     cell2.innerHTML = localStorage.getItem("option1");
-//     cell3.innerHTML = localStorage.getItem("option2")
-//     cell4.innerHTML = localStorage.getItem("option3")
-//     cell5.innerHTML = localStorage.getItem("option4")
-//     cell6.innerHTML = localStorage.getItem("correct")
-//   }
-
-const removeRow = (td)=>{
-   
-        row=td.parentElement.parentElement;
-        document.getElementById("table1").deleteRow(row.rowIndex);
-        
-      }
-    // document.getElementById("title").innerHTML=localStorage.getItem("title");
-
-    // document.getElementById("option1").innerHTML=localStorage.getItem("option1");
-    // document.getElementById("option2").innerHTML=localStorage.getItem("option2");
-    // document.getElementById("option3").innerHTML=localStorage.getItem("option3");
-    // document.getElementById("option4").innerHTML=localStorage.getItem("option4");
-    // document.getElementById("correct").innerHTML=localStorage.getItem("right");
-  
+const removeRow = (td) => {
+  row = td.parentElement.parentElement;
+  const values = JSON.parse(localStorage.getItem("obj"));
+  const remainingvalues = values.filter((element, index) => index != (row.rowIndex - 1));
+  localStorage.setItem("obj", JSON.stringify(remainingvalues));
+  document.getElementById("table1").deleteRow(row.rowIndex);
+}
